7f09e280ed9c68c525c7ed64d471d295fb22aaa2
franckfadeur <franck.fadeur@gmail.com>
2017-10-30 16:56:28 +0100

    tests en cours afin de commencer le projet à la fin du tutoriel

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,50 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('users', 'UsersController@getInfos');
+Route::post('users', 'UsersController@postInfos');
+
+
+//Route::get('article/{n}', 'ArticleController@show')->where('n', '[0-9]+');
+//
+//Route::get('facture/{n}', function($n) {
+//    return view('article')->with('numero', $n);
+//    return view('article')->withNumero($n);
+//    return view('facture', ['numero' => $n]);
+//})->where('n', '[0-9]+');
+
+
+//Route::get('article/{n}', function($n) {
+//    return view('article')->with('numero', $n);
+//    return view('article')->withNumero($n);
+//    return view('article', ['numero' => $n]);
+//})->where('n', '[0-9]+');
+
+//Route::get('/', function()
+//{
+//    return view('view1');
+//});
+
+//Route::get('1', function () {
+//    return response('Je suis la page 1 !', 200);
+//});
