4034d1b9c03772eaccf0f7a7834251d11db782aa
franckfadeur <franck.fadeur@gmail.com>
2017-11-06 15:43:34 +0100

    erreur lors du push précédent

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class DocumentController extends Controller
 {
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -6,15 +6,16 @@ use Illuminate\Http\Request;
 
 class UploadFileController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("print");
     }
 
-    public function showUploadFile(Request $request){
+    public function showUploadFile(Request $request)
+    {
         $file = $request->file('doc');
 
         //Move Uploaded File
-
         $destinationPath = env("FILE_UPLOAD", "uploads");
         $file->move($destinationPath,$file->getClientOriginalName());
 
